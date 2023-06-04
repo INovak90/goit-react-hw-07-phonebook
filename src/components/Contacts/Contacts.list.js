d1c32c66579db9875da9ce278314de756cd4c1a8
INovak90 <112341296+INovak90@users.noreply.github.com>
2023-06-04 17:38:06 +0300

    edits

--- src/components/Contacts/Contacts.list.js
+++ src/components/Contacts/Contacts.list.js
@@ -1,11 +1,7 @@
 import { ListContacts } from './Contacts.styled';
 import { ContactsCard } from './Contacts.card';
-import { useSelector } from 'react-redux';
-import { selectIsLoading } from '../../redux/selectors';
 
 export const ContactsList = () => {
-  const isLoading = useSelector(selectIsLoading);
-
   return (
     <ListContacts>
       <ContactsCard />
